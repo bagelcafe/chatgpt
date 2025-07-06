547683605eb734825678d643573f068049a0d482
Jin Y. Hur <35480129+bagelcafe@users.noreply.github.com>
2025-07-06 20:41:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')  # Ensure sqlite3 is imported to avoid import errors in some environments
-import sys
-sys.modules['sqlite3'] = sys.modules.pop['pysqlite3']
+#__import__('pysqlite3')  # Ensure sqlite3 is imported to avoid import errors in some environments
+#import sys
+#sys.modules['sqlite3'] = sys.modules.pop['pysqlite3']
 
 
 from langchain_community.document_loaders import PyPDFLoader
